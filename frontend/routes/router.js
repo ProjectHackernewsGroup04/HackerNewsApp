72ec3c8744cbb5f9310ae41d9866150cacf7ab31
Cherry Rose Semena <cph-cs241@cphbusiness.dk>
2018-09-10 16:22:42 +0200

    moving stuff is a real thing

--- frontend/routes/router.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let request = require('request');
-
-router.get('/', function(req, res) {
-  res.send('This is the hackernews frontend.');
-});
-
-module.exports = router;
